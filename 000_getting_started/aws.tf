7cdebd23be199cb709664a98c94222f857503c87
Mike Hacker <mhackersu@gmail.com>
2021-12-14 20:41:37 -0600

    instance

---
+++ 000_getting_started/aws.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "banana_instance" {
+  ami           = "ami-087c17d1fe0178315"
+  instance_type = var.instance_type
+  tags = {
+    Name = " app-server-${var.project_name}"
+  }
+}
--- 000_getting_started/variables.tf
+++ 000_getting_started/variables.tf
@@ -1,11 +1,13 @@
-# Dynamic
-variable "project_name" {
-  type        = string
-  default     = "tf-labs"
-}
-
-# Static
 variable "backend_s3" {
   type    = string
   default = "bananaforscale-aws-tf-state"
+}
+
+variable "project_name" {
+  type    = string
+  default = "tf-labs"
+}
+
+variable "instance_type" {
+  type = string
 }
