37615c000dc50ef506d3bc0e933ec0bd9cb0e46f
Mike Hacker <mhackersu@gmail.com>
2021-12-14 19:17:05 -0600

    housekeeping

---
+++ 000_getting_started/aws_instance.tf
@@ -0,0 +1,15 @@
+  # ec2.tf
+
+# resource "aws_instance" "banana" {
+#   ami           = "ami-087c17d1fe0178315"
+#   instance_type = var.instance_type
+
+#   tags = {
+#     Name = "${var.project_name}_${local.project_name}"
+#   }
+# }
+
+# locals {
+#   project_name = "frozen-banana"
+# }
+
