b28a6df2e823cf7baf1a6a2740fca045a44a2aa7
Mike Hacker <mhackersu@gmail.com>
2021-12-14 20:59:53 -0600

    ec2_instance module

--- 000_getting_started/aws.tf
+++ 000_getting_started/aws.tf
@@ -1,7 +1,18 @@
-# resource "aws_instance" "banana_instance" {
-#   ami           = "ami-087c17d1fe0178315"
-#   instance_type = var.instance_type
-#   tags = {
-#     Name = " app-server-${var.project_name}"
-#   }
-# }
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
